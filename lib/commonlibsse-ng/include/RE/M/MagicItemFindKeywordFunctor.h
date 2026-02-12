e1c48fc14f5b1a4bf249ddca82b90e37c6f678ce
Amethyst <RealAmethyst@users.noreply.github.com>
2026-02-12 11:53:03 +0100

    Initial commit from commonlibsse-ng template

---
+++ lib/commonlibsse-ng/include/RE/M/MagicItemFindKeywordFunctor.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "RE/M/MagicItemFindFunctor.h"
+
+namespace RE
+{
+	class BGSKeyword;
+
+	class MagicItemFindKeywordFunctor : public MagicItemFindFunctor
+	{
+	public:
+		inline static constexpr auto RTTI = RTTI_MagicItemFindKeywordFunctor;
+		inline static constexpr auto VTABLE = VTABLE_MagicItemFindKeywordFunctor;
+
+		~MagicItemFindKeywordFunctor() override;  // 00
+
+		// override (MagicItemFindFunctor)
+		bool MatchCondition(Effect* a_effect) override;  // 02
+
+		// members
+		BGSKeyword* keyword;  // 18
+	private:
+		KEEP_FOR_RE()
+	};
+	static_assert(sizeof(MagicItemFindKeywordFunctor) == 0x20);
+}
